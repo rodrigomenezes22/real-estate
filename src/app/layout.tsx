14169bd556e83c0ba51e1db1637064b9d12bf55b
rodrigomenezes22 <rodrigomenezes22@gmail.com>
2024-05-06 14:19:29 -0300

    Styles added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import ThemeProvider from "@/providers/theme-provider";
 
 export const metadata: Metadata = {
   title: "Rodrigo Properties",
@@ -14,7 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import { Button, Input } from "antd";
+
 export default function Home() {
-  return <div>Main</div>;
+  return (
+    <div className=" flex items-center justify-center flex-col gap-5 h-screen">
+      <h1>Home Page</h1>
+
+      <Input placeholder="Basic input" className="w-60" />
+      <Button type="primary">Primary Button</Button>
+    </div>
+  );
 }
