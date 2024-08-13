a816887e95376c256831c65a8a2a67b142d3ac49
rodrigomenezes22 <rodrigomenezes22@gmail.com>
2024-08-13 14:56:30 -0300

    added loader

--- src/providers/layout-provider.tsx
+++ src/providers/layout-provider.tsx
@@ -5,11 +5,19 @@ import { SignIn } from "@clerk/nextjs";
 import { useState, useEffect } from "react";
 import { GetCurrentUserFromMongoDB } from "@/actions/users";
 import { User } from "@prisma/client";
-
+import { usePathname } from "next/navigation";
+import Loader from "@/components/loader";
 function LayoutProvider({ children }: { children: React.ReactNode }) {
   const [currentUserData = null, setCurrentUserData] =
     React.useState<User | null>(null);
+  const [loading, setLoading] = React.useState<boolean>(false);
+
+  const pathname = usePathname();
+  const isPublicRoute = ["sign-in", "sign-up"].includes(pathname.split("/")[1]);
+
   const getHeader = () => {
+    if (isPublicRoute) return null;
+    if (loading) return <Loader />;
     return (
       <div className="lg:px-20 px-5">
         <div className="bg-primary p-4 flex justify-between items-center rounded-b">
@@ -25,19 +33,25 @@ function LayoutProvider({ children }: { children: React.ReactNode }) {
   };
 
   const getContent = () => {
+    if (isPublicRoute) return children;
     return <div className="py-5 lg:px-20 px-5">{children}</div>;
   };
 
   const getCurrentUser = async () => {
     try {
+      setLoading(true);
       const response: any = await GetCurrentUserFromMongoDB();
       if (response.error) throw new Error(response.error);
       setCurrentUserData(response.data);
-    } catch (error: any) {}
+    } catch (error: any) {
+      setLoading(false);
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
-    getCurrentUser();
+    if (!isPublicRoute) getCurrentUser();
   }, []);
 
   return (
